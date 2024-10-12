0b5076f9758e2f75abfd70b2a870fd26e3cb38d4
N Rizki Amalia <rizkiamel9@gmail.com>
2024-10-12 16:56:08 +0700

    ok fix

--- components/Music/index.tsx
+++ components/Music/index.tsx
@@ -16,46 +16,40 @@ declare global {
 
 const Music = forwardRef<any, MusicProps>(({ onReady }, ref) => {
   const [isMuted, setIsMuted] = useState(false);
-  const [isPlayerLoaded, setIsPlayerLoaded] = useState(false); // To track if the player is loaded
   const [player, setPlayer] = useState<YT.Player | null>(null);
 
-  // Function to load YouTube API and initialize player
-  const loadYouTubePlayer = () => {
-    if (!isPlayerLoaded) {
-      const tag = document.createElement("script");
-      tag.src = "https://www.youtube.com/iframe_api";
-      const firstScriptTag = document.getElementsByTagName("script")[0];
-      firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
+  useEffect(() => {
+    const tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
 
-      window.onYouTubeIframeAPIReady = () => {
-        const newPlayer = new window.YT.Player("music-player", {
-          videoId: "cpYYX3ONPGc", // Replace with your video ID
-          events: {
-            onReady: (event: YT.PlayerEvent) => {
-              event.target.setVolume(50);
-              onReady(); // Notify that the player is ready
-            },
+    window.onYouTubeIframeAPIReady = () => {
+      const newPlayer = new window.YT.Player("music-player", {
+        videoId: "cpYYX3ONPGc", // Replace with your video ID
+        events: {
+          onReady: (event: YT.PlayerEvent) => {
+            event.target.setVolume(50);
+            onReady(); // Notify that the player is ready
           },
-          playerVars: {
-            autoplay: 0, // Autoplay off
-            loop: 1,
-            playlist: "jtK-xIM-7tU",
-            controls: 0,
-            showinfo: 0,
-            modestbranding: 1,
-          },
-        });
-        setPlayer(newPlayer);
-        setIsPlayerLoaded(true); // Mark the player as loaded
-      };
-    }
-  };
+        },
+        playerVars: {
+          autoplay: 0, // Autoplay off
+          loop: 1,
+          playlist: "jtK-xIM-7tU",
+          controls: 0,
+          showinfo: 0,
+          modestbranding: 1,
+        },
+      });
+      setPlayer(newPlayer);
+    };
+  }, [onReady]);
 
-  // Play music function exposed via ref
   useImperativeHandle(ref, () => ({
     playMusic: () => {
       if (player) {
-        player.playVideo(); // Play video when called
+        player.playVideo(); // Play the video when called
       }
     }
   }));
